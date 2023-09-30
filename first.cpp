d0bce6d100a9eda0e4eb1445c5e99fece8f8a73c
mohitrao111 <raomohit142625@gmail.com>
2023-09-30 21:57:10 +0530

    added first.cpp

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+    cin>>name;
+    cin>>age;
+    cout<<"My name is :"<<name<<endl;
+    cout<<"My age is :"<<age;
+    }
